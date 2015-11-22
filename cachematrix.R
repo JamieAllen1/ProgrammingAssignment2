88e60808d2784fa7d66de1b84e69ea5492bd1474
JamieAllen1 <jamie_allen1@hotmail.com>
2015-11-22 21:02:28 +1000

    Added comments and tidied up code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions allow for the caching of a matrix for further operations and 
+## also for the calculation and caching of the inverse of the matrix initially passed. 
 
-## Write a short comment describing this function
+## The makeCacheMatrix function does nothing but holds a matrix initially passed
+## to, returns it when requested and does the same for the inverse of the same martix.
+## The function returns a list of the actual functions that are called.
 
 makeCacheMatrix <- function(x = matrix()) {
-  M <- x
-  invM <- NULL
+  M <- x ## The Matrix
+  invM <- NULL ## The Inverse of the Matrix
+  
+  ## Saves the Matrix
   push <- function(y){
     M <<- y
     InvM <<- NULL
   }
-  pop <- function() M
-  pushInv <- function(m) invM <<- m
-  popInv <- function() invM
+  pop <- function() M ## Returns the Matrix
+  pushInv <- function(m) invM <<- m ## Saves the Inverse
+  popInv <- function() invM  ## Returns the Inverse
   
   list(pop = pop, push = push, popInv = popInv, pushInv = pushInv)
 
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function firstly checks to see if the Inverse
+## of the Martix exists. If it does it returns the Inverse.
+## If not, it returns the value of the Matrix, calculates the Inverse
+### using solve(), pushes it to the cache function and then returns the inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
